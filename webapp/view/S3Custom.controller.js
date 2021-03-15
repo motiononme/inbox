17b01e2a5d03819eaf3f4ee0fac44e146e7fbae8
motiononme <matrixsandeep2007@gmail.com>
2021-03-15 12:01:53 +0530

    Initial Push

---
+++ webapp/view/S3Custom.controller.js
@@ -0,0 +1,8 @@
+sap.ui.controller("cross.fnd.fiori.inbox.CA_FIORI_INBOXExtension1.view.S3Custom", {
+
+	extHookGetEntitySetsToExpand: function() {
+		// Place your hook implementation code here 
+		debugger;
+	}
+
+});
